727443306c66ae30af9bb0a13018634a512b9aef
Crowdbotics <team@crowdbotics.com>
2020-11-09 23:17:13 +0000

    Update models

--- backend/h2go_22455/wsgi.py
+++ backend/h2go_22455/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'h2go_22455.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "h2go_22455.settings")
 
 application = get_wsgi_application()
