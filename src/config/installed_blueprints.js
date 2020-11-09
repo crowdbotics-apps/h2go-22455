1a7dbdd78badaca2c1b1fe900333061d4fe15951
Crowdbotics <team@crowdbotics.com>
2020-11-09 23:18:25 +0000

    add package blueprint-react-native-maps-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'Maps170062', human_name: 'Maps', access_route: 'Maps170062', icon: 'map'},
 { name: 'Settings170040', human_name: 'Settings', access_route: 'Settings170040'},
 { name: 'Settings170025', human_name: 'Settings', access_route: 'Settings170025'},
 { name: 'NotificationList170024', human_name: 'Notification List', access_route: 'NotificationList170024'},
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import Maps170062Navigator from '../features/Maps170062/navigator';
 import Settings170040Navigator from '../features/Settings170040/navigator';
 import Settings170025Navigator from '../features/Settings170025/navigator';
 import NotificationList170024Navigator from '../features/NotificationList170024/navigator';
@@ -17,6 +18,7 @@ import Maps170023Navigator from '../features/Maps170023/navigator';
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+Maps170062: { screen: Maps170062Navigator },
 Settings170040: { screen: Settings170040Navigator },
 Settings170025: { screen: Settings170025Navigator },
 NotificationList170024: { screen: NotificationList170024Navigator },
